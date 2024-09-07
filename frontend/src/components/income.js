b78f1cf943ecd1e3e48e8d9036c1f6862a06ae4f
Kate Yago <k.kud@bk.ru>
2024-09-07 17:07:10 +0400

    created commit

--- frontend/src/components/income.js
+++ frontend/src/components/income.js
@@ -1,5 +1,13 @@
 export class Income {
     constructor() {
-        console.log(1);
+        this.popupDelete();
+    }
+    popupDelete() {
+        document.getElementById('delete-popup-open').addEventListener('click', function ()  {
+          document.getElementById('delete-popup').style.display = 'block';
+          document.getElementById('content').style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+          document.getElementById('sidebar').style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+          document.getElementById('content').style.zIndex = '99';
+        });
     }
 }
