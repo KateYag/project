746777480d3f272d63523242f17a2f79f618f69c
Kate Yago <k.kud@bk.ru>
2024-09-09 21:32:37 +0400

    popup

--- frontend/src/components/income-expenses.js
+++ frontend/src/components/income-expenses.js
@@ -1,4 +1,25 @@
 export class IncomeExpenses {
     constructor() {
+        this.popupDelete();
+        this.notDeleteElement();
+    }
+    popupDelete() {
+        const element = document.getElementsByClassName('delete-popup-open');
+        for (let i = 0; i < element.length; i++) {
+            element[i].addEventListener('click', () =>  {
+                document.getElementById('delete-popup').style.display = 'block';
+                document.getElementById('overlay').style.display = 'block';
+            });
+        }
+    }
+    notDeleteElement() {
+        const notDeleteElement = document.getElementById('not-delete');
+        if (notDeleteElement) {
+            notDeleteElement.addEventListener('click', () => {
+                document.getElementById('delete-popup').style.display = 'none';
+                document.getElementById('overlay').style.display = 'none';
+
+            })
+        }
     }
 }
